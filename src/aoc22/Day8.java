583db3a94c3bbc25689e061aacdfdd6fa23a8fab
grovepond <grovepond@protonmail.com>
2022-12-08 22:07:14 +0100

    Day 8 task two completed

--- src/aoc22/Day8.java
+++ src/aoc22/Day8.java
@@ -7,10 +7,10 @@ import java.util.*;
 public class Day8 {
 
     private final List<String> input;
-    private static final String VP_WEST = "W";
-    private static final String VP_EAST = "E";
-    private static final String VP_NORTH = "N";
-    private static final String VP_SOUTH = "S";
+    private static final String _LEFT = "L";
+    private static final String _RIGHT = "R";
+    private static final String _UP = "U";
+    private static final String _DOWN = "D";
 
 
 
@@ -25,10 +25,10 @@ public class Day8 {
         int visibleCount = 0;
         for (int i = 0; i < grid.size(); i++) {
             for (int j = 0; j < grid.get(0).size(); j++) {
-                if (isVisible(grid, i, j, VP_WEST) ||
-                        isVisible(grid, i, j, VP_EAST) ||
-                        isVisible(grid, i, j, VP_NORTH) ||
-                        isVisible(grid, i, j, VP_SOUTH))
+                if (isVisible(grid, i, j, _LEFT) ||
+                        isVisible(grid, i, j, _RIGHT) ||
+                        isVisible(grid, i, j, _UP) ||
+                        isVisible(grid, i, j, _DOWN))
                     visibleCount++;
             }
         }
@@ -36,11 +36,11 @@ public class Day8 {
     }
 
     public String run2 () { // Couldn't fix this yet. Doesn't work.
-        var grid = parseInput();
         int score, highScore = 0;
+        var grid = parseInput();
         for (int i = 0; i < grid.size(); i++) {
             for (int j = 0; j < grid.get(i).size(); j++) {
-                score = getViewingScore(grid, i, j);
+                score = getScenicDistance(i, j, grid);
                 if (score > highScore)
                     highScore = score;
             }
@@ -52,28 +52,28 @@ public class Day8 {
             return true;
         }
         switch (vantagePoint) {
-            case VP_WEST -> {
+            case _LEFT -> {
                 for (int i = row + 1; i < grid.size(); i++) {
                     if (grid.get(i).get(col) >= grid.get(row).get(col)) {
                         return false;
                     }
                 }
             }
-            case VP_EAST -> {
+            case _RIGHT -> {
                 for(int i = row - 1; i >= 0; i--) {
                     if(grid.get(i).get(col) >= grid.get(row).get(col)) {
                         return false;
                     }
                 }
             }
-            case VP_NORTH -> {
+            case _UP -> {
                 for(int i = col + 1; i < grid.get(0).size(); i++) {
                     if(grid.get(row).get(i) >= grid.get(row).get(col)) {
                         return false;
                     }
                 }
             }
-            case VP_SOUTH -> {
+            case _DOWN -> {
                 for(int i = col - 1; i >= 0; i--) {
                     if(grid.get(row).get(i) >= grid.get(row).get(col)) {
                         return false;
@@ -84,34 +84,7 @@ public class Day8 {
         return true;
     }
 
-    private int getViewingScore(List<List<Integer>> grid, int row, int col) {
-        int westScore = 0, eastScore = 0, northScore = 0, southScore = 0;
-                for (int i = row + 1; i < grid.size(); i++) {
-                    if (grid.get(i).get(col) < grid.get(row).get(col))
-                        eastScore++;
-                    else
-                        break;
-                }
-                for (int i = row - 1; i > 0; i--) {
-                    if (grid.get(i).get(col) < grid.get(row).get(col))
-                        westScore++;
-                    else
-                        break;
-                }
-                for (int i = col + 1; i < grid.get(col).size(); i++) {
-                    if (grid.get(row).get(i) < grid.get(row).get(col))
-                        northScore++;
-                    else
-                        break;
-                }
-                for (int i = col - 1; i > 0; i--) {
-                    if (grid.get(row).get(i) < grid.get(row).get(col))
-                        southScore++;
-                    else
-                        break;
-                }
-        return eastScore * westScore * northScore * southScore;
-    }
+
 
 
 
@@ -126,5 +99,54 @@ public class Day8 {
     }
 
 
+    private static int getScenicDistance(int row, int col, List<List<Integer>> grid) {
+        if (row == 0 || col == 0) {
+            return 0;
+        }
 
+        int rowMax = grid.get(col).size() - 1;
+        int colMax = grid.size() - 1;
+
+        if (row == rowMax || col == colMax) {
+            return 0;
+        }
+
+        int height = grid.get(col).get(row);
+        int upDistance, downDistance, rightDistance, leftDistance;
+
+        upDistance = row;
+        for (int i = row - 1; i >= 0; i--) {
+            if (grid.get(col).get(i) >= height) {
+                upDistance = row - i;
+                break;
+            }
+        }
+
+        downDistance = rowMax - row;
+        for (int i = row + 1; i <= rowMax; i++) {
+            if (grid.get(col).get(i) >= height) {
+                downDistance = i - row;
+                break;
+            }
+        }
+
+
+        rightDistance = col;
+        for (int i = col - 1; i >= 0; i--) {
+            if (grid.get(i).get(row) >= height) {
+                rightDistance = col - i;
+                break;
+            }
+        }
+
+        leftDistance = colMax - col;
+        for (int i = col + 1; i <= colMax; i++) {
+            if (grid.get(i).get(row) >= height) {
+                leftDistance = i - col;
+                break;
+            }
+        }
+
+        return upDistance * downDistance * rightDistance * leftDistance;
     }
+}
